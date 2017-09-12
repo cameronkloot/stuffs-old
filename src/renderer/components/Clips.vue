53bab2711d43d4bb22c784a3e31bbe204267e959
cameronkloot <cameron@0ptimus.com>
2017-09-12 12:26:16 -0400

    Add clip selection

--- src/renderer/components/Clips.vue
+++ src/renderer/components/Clips.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="clips">
     <div class="header">
-      <input
-        class="command"
+      <input class="command"
+        ref="command"
         type="text"
         placeholder="Type here..."
         v-model="command"
-        @keyup.enter="clickAddClip">
+        @keydown.enter="clickAddClip"
+        @focus="selected = -1">
       </input>
       <button class="add"
         v-show="command.length > 0"
@@ -14,8 +15,13 @@
         ADD
       </button>
     </div>
-    <div class="clips-container">
-      <div v-for="clip in clips" :key="clip.id" class="clip">
+    <div class="clips-container" ref="list">
+      <div class="clip"
+        v-for="(clip, index) in clips"
+        :key="clip.id"
+        ref="clips"
+        :data-selected="selected === index"
+        :selected="selected === index">
         <span class="text">{{ clip.text }}</span>
         <span class="buttons">
           <button class="remove" @click="clickRemoveClip(clip)">X</button>
@@ -51,6 +57,40 @@ const methods = {
   },
   clickRemoveClip (clip) {
     this.removeClip(clip)
+  },
+  keydownSelectClips ($event) {
+    if ($event.key !== 'ArrowUp' && $event.key !== 'ArrowDown') {
+      return
+    }
+
+    // Increment/decrement selection
+    if ($event.key === 'ArrowUp') {
+      this.selected = this.selected > -1 ? this.selected - 1 : -1
+    } else if ($event.key === 'ArrowDown') {
+      this.selected = this.selected < this.clips.length - 1 ? this.selected + 1 : this.selected
+    }
+
+    if (this.selected === -1) {
+      // Focus on command input
+      this.$refs.command.focus()
+    } else {
+      this.$refs.command.blur()
+
+      // Scroll to keep selected in view
+      const command = this.$refs.command
+      const list = this.$refs.list
+      const clip = this.$refs.clips[this.selected]
+
+      let scrollTop = list.scrollTop
+      if (clip.offsetTop + clip.offsetHeight >
+          list.scrollTop + list.offsetHeight + command.offsetHeight) {
+        scrollTop += clip.offsetHeight
+        list.scrollTop = scrollTop > list.scrollHeight ? list.scrollHeight : scrollTop
+      } else if (clip.offsetTop < list.scrollTop + command.offsetHeight) {
+        scrollTop -= clip.offsetHeight
+        list.scrollTop = scrollTop < 0 ? 0 : scrollTop
+      }
+    }
   }
 }
 
@@ -60,8 +100,19 @@ export default {
   methods,
   data () {
     return {
-      command: ''
+      command: '',
+      selected: -1
     }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      window.addEventListener('keydown', this.keydownSelectClips)
+    })
+  },
+  destroyed () {
+    this.$nextTick(() => {
+      window.removeEventListener('keydown', this.keydownSelectClips)
+    })
   }
 }
 </script>
