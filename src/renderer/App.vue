de7f4df2f29e1a9176c73cf07a70c5d12ac0f9c1
cameronkloot <cameron@0ptimus.com>
2017-12-10 20:28:27 -0500

    Move clipboard watching to mixin

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -6,17 +6,16 @@
 </template>
 
 <script>
-import { clipboard } from 'electron'
-import { mapActions } from 'vuex'
+
+import clipboardMixin from '@/mixins/clipboard'
 import Sidebar from '@/components/Sidebar'
 
 const name = 'stuffs'
 
-const methods = {
-  ...mapActions('clips', [
-    'add'
-  ])
-}
+const mixins = [
+  clipboardMixin
+]
+
 
 const components = {
   Sidebar
@@ -24,44 +23,10 @@ const components = {
 
 export default {
   name,
-  methods,
+  mixins,
   components,
   mounted () {
-    const context = this
-    let lastText = clipboard.readText()
-    let lastImage = clipboard.readImage()
-    // let lastImageDataUrl = lastImage.toDataURL()
-    let lastImageSize = lastImage.getSize()
-
-    const checkImage = (image) => {
-      const size = image.getSize()
-      // add data url check if size matches
-      return size.width !== lastImageSize.width &&
-        size.height !== lastImageSize.height
-    }
-
-    setInterval(() => {
-      const text = clipboard.readText()
-      const image = clipboard.readImage()
-      if (image.isEmpty() === false && checkImage(image) === true) {
-        context.add({
-          text,
-          image,
-          type: 'image',
-          source: 'clipboard'
-        })
-        lastImage = image
-        // lastImageDataUrl = image.toDataURL()
-        lastImageSize = image.getSize()
-      } else if (text && text !== lastText) {
-        context.add({
-          text,
-          type: 'text',
-          source: 'clipboard'
-        })
-        lastText = text
-      }
-    }, 100)
+    this.watchClipboard()
   }
 }
 </script>
---
+++ src/renderer/mixins/clipboard.js
@@ -0,0 +1,50 @@
+import { clipboard } from 'electron'
+import { mapActions } from 'vuex'
+
+const methods = {
+  ...mapActions('clips', [
+    'add'
+  ]),
+  watchClipboard () {
+    const self = this
+    let lastText = clipboard.readText()
+    let lastImage = clipboard.readImage()
+    // let lastImageDataUrl = lastImage.toDataURL()
+    let lastImageSize = lastImage.getSize()
+
+    const checkImage = (image) => {
+      const size = image.getSize()
+      // add data url check if size matches
+      return size.width !== lastImageSize.width &&
+        size.height !== lastImageSize.height
+    }
+
+    setInterval(() => {
+      const text = clipboard.readText()
+      const image = clipboard.readImage()
+      if (image.isEmpty() === false && checkImage(image) === true) {
+        self.add({
+          text,
+          image,
+          type: 'image',
+          source: 'clipboard'
+        })
+        lastImage = image
+        // lastImageDataUrl = image.toDataURL()
+        lastImageSize = image.getSize()
+      } else if (text && text !== lastText) {
+        self.add({
+          text,
+          type: 'text',
+          source: 'clipboard'
+        })
+        lastText = text
+      }
+    }, 100)
+
+  }
+}
+
+export default {
+  methods
+}
